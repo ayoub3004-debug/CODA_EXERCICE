afdf55531be0f0201026325ed473b51343cd1483
ayoub3004-debug <ayoub.nader3004@gmail.com>
2025-10-02 15:39:22 +0200

    Exercice3

--- OneDrive/CODA SCHOOL/CODA EXO/Main.py
+++ OneDrive/CODA SCHOOL/CODA EXO/Main.py
@@ -6,23 +6,24 @@ def exercice1():
     print("Hello World !")
 
 def exercice2():
-   any = input("exercice 2 : entrer votre prénom ?")
-   print("Bonjour", any)
+   prenom = input("exercice 2 : entrer votre prénom ?")
+   print("Bonjour", prenom)
 
 def exercice3():
     print("première ligne\ndeuxieme ligne\ntroisieme ligne")
-    
    
 def main():
     # Demande à l'utilisateur quel exercice exécuter
     choix = input("Entrez le numéro de l'exercice à exécuter : ")
     if choix == "1":
         exercice1()
-    if choix == "2":
+    elif choix == "2":
         exercice2()
-    if choix == "3":
+    elif choix == "3":
         exercice3()
     else:
         print("Exercice non reconnu.")
+        
 if __name__ == "__main__":
+
     main()
