70dff52b1e0f03a8fba76c806b354190ca47b926
ayoub3004-debug <ayoub.nader3004@gmail.com>
2025-10-02 15:46:36 +0200

    Exercice4

--- OneDrive/CODA SCHOOL/CODA EXO/Main.py
+++ OneDrive/CODA SCHOOL/CODA EXO/Main.py
@@ -1,4 +1,5 @@
 from ast import main
+from datetime import date
 
 
 def exercice1():
@@ -11,6 +12,13 @@ def exercice2():
 
 def exercice3():
     print("première ligne\ndeuxieme ligne\ntroisieme ligne")
+
+def exercice4():
+    annee = int(input("Entrez votre année de naissance : "))
+    annee_actuelle = date.today().year
+    age = annee_actuelle - annee
+    print("Vous avez environ", age, "ans.")
+
    
 def main():
     # Demande à l'utilisateur quel exercice exécuter
@@ -21,9 +29,11 @@ def main():
         exercice2()
     elif choix == "3":
         exercice3()
+    elif choix == "4":
+        exercice4()
     else:
         print("Exercice non reconnu.")
-        
+
 if __name__ == "__main__":
 
     main()
